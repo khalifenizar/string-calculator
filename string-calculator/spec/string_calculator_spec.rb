3989e0415f27df3bb2fabc4632eff328a055e934
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-01-18 12:37:14 -0500

    Factor out calculator instace with before

--- string-calculator/spec/string_calculator_spec.rb
+++ string-calculator/spec/string_calculator_spec.rb
@@ -3,29 +3,28 @@
 require_relative("../lib/string_calculator.rb")
 
 RSpec.describe StringCalculator do
+  before :each do
+    @my_calc = StringCalculator.new
+  end
+
   it "returns 0 for empty string" do
-    my_calc = StringCalculator.new
     # p my_calc.add( "" ) == 0
-    expect(my_calc.add( "" )).to eq(0)
+    expect(@my_calc.add( "" )).to eq(0)
   end
 
   it "returns the number for one number" do
-    my_calc = StringCalculator.new
-
     # p my_calc.add( "9" ) == 9
-    expect(my_calc.add( "9" )).to eq(9)
+    expect(@my_calc.add( "9" )).to eq(9)
 
     # p my_calc.add( "5" ) == 5
-    expect(my_calc.add( "5" )).to eq(5)
+    expect(@my_calc.add( "5" )).to eq(5)
   end
 
   it "returns sum two numbers" do
-    my_calc = StringCalculator.new
-
     # p my_calc.add( "2,5" ) == 7
-    expect(my_calc.add( "2,5" )).to eq(7)
+    expect(@my_calc.add( "2,5" )).to eq(7)
 
     # p my_calc.add( "8,8" ) == 16
-    expect(my_calc.add( "8,8" )).to eq(16)
+    expect(@my_calc.add( "8,8" )).to eq(16)
   end
 end
